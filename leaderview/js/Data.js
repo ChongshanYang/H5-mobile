f14f0a435fd738901d230c2151f91e01b464791f
yanglc <yangliucheng1994@foxmail.com>
2016-04-13 14:41:57 +0800

    add leaderview files

---
+++ leaderview/js/Data.js
@@ -0,0 +1,3 @@
+﻿var jasonData = {
+    "data": [{ "datacollection": [{ "title": "李志杰", "amount": "22" }, { "title": "李欣", "amount": "18" }, { "title": "刘真", "amount": "14" }, { "title": "田华欣", "amount": "10" }, { "title": "朱杰飞", "amount": "6" }] }]    
+};
